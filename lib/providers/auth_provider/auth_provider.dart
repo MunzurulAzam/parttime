4b89a9b020a81f88b0f05f879745d4904af0aec6
MunzurulAzam <52912582+MunzurulAzam@users.noreply.github.com>
2024-09-28 03:23:56 +0600

    ...

--- lib/providers/auth_provider/auth_provider.dart
+++ lib/providers/auth_provider/auth_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,30 +13,24 @@ class AuthProvider extends ChangeNotifier {
   //!--------------------------------firebase auth login
   Future<User?> signInWithGoogle() async {
     try {
-      // Trigger the Google authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
       if (googleUser == null) {
-        // The user canceled the sign-in
         return null;
       }
 
-      // Obtain the auth details from the request
       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-      // Create a new credential
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      // Once signed in, return the UserCredential
       return (await FirebaseAuth.instance.signInWithCredential(credential)).user;
     } catch (e) {
-      print(e.toString());
+      log(e.toString());
       return null;
     }
   }
-
 
 
 }
