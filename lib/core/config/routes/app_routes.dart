75b6df6071945e7bed3843013ed31e7ebc0bd3ec
MunzurulAzam <52912582+MunzurulAzam@users.noreply.github.com>
2024-09-28 15:00:52 +0600

    ...

--- lib/core/config/routes/app_routes.dart
+++ lib/core/config/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hotel_management/presentations/screens/auth/login_screen.dart';
 import 'package:hotel_management/presentations/screens/auth/sign_up_screen.dart';
 import 'package:hotel_management/presentations/screens/auth/starter.dart';
+import 'package:hotel_management/presentations/screens/details_page/details_screen.dart';
 import 'package:hotel_management/presentations/screens/home/home_screen.dart';
 import 'package:hotel_management/presentations/screens/home/navigation_screen.dart';
 import 'package:hotel_management/presentations/screens/profile/edit_profile.dart';
@@ -50,26 +51,26 @@ class AppRoutes {
         return MaterialPageRoute(
           builder: (_) => const DetailsScreen(),
         );
-      case RouteName.login:
-        return MaterialPageRoute(
-          builder: (_) => const LogInScreen(),
-        );
-      case RouteName.signUp:
-        return MaterialPageRoute(
-          builder: (_) => const SignUpScreen(),
-        );
-      case RouteName.editProfile:
-        return MaterialPageRoute(
-          builder: (_) => const EditProfileScreen(),
-        );
-      case RouteName.login:
-        return MaterialPageRoute(
-          builder: (_) => const LogInScreen(),
-        );
-      case RouteName.signUp:
-        return MaterialPageRoute(
-          builder: (_) => const SignUpScreen(),
-        );
+      // case RouteName.login:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const LogInScreen(),
+      //   );
+      // case RouteName.signUp:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const SignUpScreen(),
+      //   );
+      // case RouteName.editProfile:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const EditProfileScreen(),
+      //   );
+      // case RouteName.login:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const LogInScreen(),
+      //   );
+      // case RouteName.signUp:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const SignUpScreen(),
+      //   );
       // case Routes.home:
       //   final AppArguments arguments = settings.arguments as AppArguments;
       //   return MaterialPageRoute(
