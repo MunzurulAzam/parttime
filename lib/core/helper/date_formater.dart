ce2041d0b64ebef34fd0716c9941db8922ed2d87
MunzurulAzam <52912582+MunzurulAzam@users.noreply.github.com>
2024-10-05 22:47:09 +0600

    booking list data add done

--- lib/core/helper/date_formater.dart
+++ lib/core/helper/date_formater.dart
@@ -4,7 +4,11 @@ String formatDate(String timestamp) {
 
   DateTime dateTime = DateTime.parse(timestamp);
 
+<<<<<<< HEAD
   String formattedDate = DateFormat('d MMM yyyy h a').format(dateTime);
+=======
+  String formattedDate = DateFormat('d MMM yyyy').format(dateTime);
+>>>>>>> 834c035 (booking list data add done)
 
   return formattedDate;
 }
--- lib/presentations/screens/trip/trip_screen.dart
+++ lib/presentations/screens/trip/trip_screen.dart
@@ -1,11 +1,23 @@
+<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+=======
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+>>>>>>> 834c035 (booking list data add done)
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hotel_management/core/constants/colors/app_colors.dart';
 import 'package:hotel_management/core/helper/date_formater.dart';
+<<<<<<< HEAD
 import 'package:hotel_management/presentations/widgets/custom_divider_bar.dart';
 import 'package:hotel_management/providers/booking_provider/booking_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+=======
+import 'package:hotel_management/presentations/widgets/on_process_button.dart';
+import 'package:hotel_management/providers/booking_provider/booking_provider.dart';
+>>>>>>> 834c035 (booking list data add done)
 
 class TripScreens extends ConsumerStatefulWidget {
   const TripScreens({super.key});
@@ -18,120 +30,101 @@ class _TripScreensState extends ConsumerState<TripScreens> {
   @override
   Widget build(BuildContext context) {
     final bookingProvider0 = ref.watch(bookingProvider.notifier);
+<<<<<<< HEAD
 
+=======
+>>>>>>> 834c035 (booking list data add done)
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black ,
-        title: const AutoSizeText('Booking List', style: TextStyle(color: Colors.white),),),
+        backgroundColor: Colors.black,
+        title: const AutoSizeText(
+          'Booking List',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
       backgroundColor: Colors.black,
-      body:                 Padding(
-        padding:  EdgeInsets.only(top: 10.h),
-        child: ListView.builder(
+      body: Padding(
+        padding: EdgeInsets.only(top: 10.h),
+        child: FutureBuilder(
+            future: bookingProvider0.fetchBookings(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                final bookings = bookingProvider0.bookings;
+                return Padding(
+                  padding:  EdgeInsets.symmetric(horizontal: 10.w),
+                  child: ListView.builder(
                     padding: EdgeInsets.zero,
                     physics: const AlwaysScrollableScrollPhysics(),
                     shrinkWrap: true,
-                    itemCount: 6,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.only(bottom: 10.h),
-                        child: Container(
-                          height: 250.h,
-                          decoration: BoxDecoration(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(10.r),
-                          ),
-                          child: Stack(
-                            children: [
-                              Column(
-                                children: [
-                                  Container(
-                                    height: 210.h,
-                                    decoration: BoxDecoration(
-                                      color: Colors.transparent,
-                                      borderRadius: BorderRadius.circular(10.r),
-                                    ),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10.r),
-                                      child: Image.network(
-                                        width: double.infinity,
-                                         '',
-                                        fit: BoxFit.fill,
-                                        errorBuilder: (context, error, stackTrace) {
-                                          return Container(
-                                            color: Colors.grey,
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Positioned(
-                                bottom: 7.h,
-                                left: 0, // Position it to the left
-                                right: 0, // Position it to the right
-                                child: Container(
-                                  height: 55.h,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.kWhiteColor,
-                                    borderRadius: BorderRadius.circular(15.r),
-                                  ),
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(horizontal: 15.w),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        // Column(
-                                        //   mainAxisAlignment: MainAxisAlignment.center,
-                                        //   children: [
-                                        //     AutoSizeText(
-                                        //        'aaa',
-                                        //       style: TextStyle(fontSize: 13.sp, color: AppColors.kPrimaryColor),
-                                        //     ),
-                                        //     AutoSizeText(
-                                        //       '\$/ day',
-                                        //       style: TextStyle(fontSize: 13.sp, color: AppColors.kPrimaryColor),
-                                        //     ),
-                                        //   ],
-                                        // ),
-                                       
-                                        OnProcessButtonWidget(
-                                          contentPadding: EdgeInsets.symmetric(horizontal: 50.w),
-                                          height: 20.h,
-                                          backgroundColor: AppColors.kPrimaryColor,
-                                          child: AutoSizeText(
-                                            'Booked',
-                                            style: TextStyle(fontSize: 13.sp, color: AppColors.kWhiteColor),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              // Positioned(
-                              //     top: 15.h,
-                              //     right: 15.w,
-                              //     child: Container(
-                              //       padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 5.h),
-                              //       decoration: BoxDecoration(
-                              //         color: AppColors.kWhiteColor,
-                              //         borderRadius: BorderRadius.circular(20.r),
-                              //       ),
-                              //       child: Icon(
-                              //         Icons.favorite,
-                              //         color: AppColors.kPrimaryColor,
-                              //         size: 20.r,
-                              //       ),
-                              //     ))
-                            ],
-                          ),
+                    itemCount: bookings.length,
+                    itemBuilder: (context, index) {       
+                      final booking = bookings[index];
+                      String formattedDateStart = formatDate(booking['booking_start_date']);
+                      String formattedDateEnd = formatDate(booking['booking_end_date']);
+                      return OnProcessButtonWidget(
+                        borderRadius: BorderRadius.circular(15.r),
+                        backgroundColor: AppColors.kWhiteColor,
+                        margin: EdgeInsets.only(bottom: 10.h),
+                        enable: false,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            5.verticalSpace,
+                            Row(
+                              children: [
+                                AutoSizeText('Name : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' ${booking['user_name']}', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                            5.verticalSpace,
+                             Row(
+                              children: [
+                                AutoSizeText('Email : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp), maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' ${booking['user_email']}', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                            5.verticalSpace,
+                             Row(
+                              children: [
+                                AutoSizeText('Total Amount : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp), maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' ${booking['total_amount']}', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp), maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                            5.verticalSpace,
+                             Row(
+                              children: [
+                                AutoSizeText('Start Date : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' $formattedDateStart', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                            5.verticalSpace,
+                             Row(
+                              children: [
+                                AutoSizeText('End Date : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' $formattedDateEnd', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                             5.verticalSpace,
+                             Row(
+                              children: [
+                                AutoSizeText('Day Count : ', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 17.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                                AutoSizeText(' ${booking['day_count']}', style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.bold, fontSize: 16.sp),maxLines: 1, overflow: TextOverflow.clip,),
+                              ],
+                            ),
+                            5.verticalSpace,
+                          ],
                         ),
                       );
                     },
                   ),
+                );
+              }
+            }),
       ),
-              
     );
   }
 }
