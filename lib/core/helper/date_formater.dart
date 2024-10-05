bc77f9f4ce37564ed3a7064b200ec27a2b7fe0be
MunzurulAzam <52912582+MunzurulAzam@users.noreply.github.com>
2024-10-05 23:44:34 +0600

    ....

--- lib/core/helper/date_formater.dart
+++ lib/core/helper/date_formater.dart
@@ -4,7 +4,7 @@ String formatDate(String timestamp) {
 
   DateTime dateTime = DateTime.parse(timestamp);
 
-  String formattedDate = DateFormat('d MMM yyyy').format(dateTime);
+  String formattedDate = DateFormat('d MMM yyyy h a').format(dateTime);
 
   return formattedDate;
 }
