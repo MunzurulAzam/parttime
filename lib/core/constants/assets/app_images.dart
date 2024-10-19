1d6d009b3e40bf260ba30721f1a9a3e6ba1aad64
morshed-islam <morshedislam21@gmail.com>
2024-10-19 22:16:45 +0600

    images change

--- lib/core/constants/assets/app_images.dart
+++ lib/core/constants/assets/app_images.dart
@@ -3,7 +3,7 @@ import 'package:hotel_management/core/extensions/extensions.dart';
 class AppImages {
   ///png, jpej and jpg image path set
   static String get placeholder => 'placeholder'.png;
-  static String get logo => 'logo'.png;
+  static String get logo => 'logo2'.png;
   static String get bcImage => 'bcImage2'.jpg;
   static String get backInage => 'back'.jpg;
   static String get bg => 'bg'.jpeg;
